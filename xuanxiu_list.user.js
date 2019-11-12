c74de5b5ff2b696dba8cb84a094e5577dbfb62b3
Your Name <27681135@qq.com>
2019-11-12 12:10:06 +0800

    列表页增加判断是否页面激活，激活则提前结束计时

--- xuanxiu_list.user.js
+++ xuanxiu_list.user.js
@@ -9,17 +9,48 @@
 // ==/UserScript==
 
 (function() {
-  "use strict";
+  ("use strict");
+
   function sleepTime(secends) {
     return new Promise(resolve => {
       setTimeout(() => resolve(), secends * 1000);
     });
   }
+  let isCurrentPageActive = false;
+  // 不同浏览器 hidden 名称
+  var hiddenProperty =
+    "hidden" in document
+      ? "hidden"
+      : "webkitHidden" in document
+      ? "webkitHidden"
+      : "mozHidden" in document
+      ? "mozHidden"
+      : null;
+  // 不同浏览器的事件名
+  var visibilityChangeEvent = hiddenProperty.replace(
+    /hidden/i,
+    "visibilitychange"
+  );
+  var onVisibilityChange = function() {
+    if (document[hiddenProperty]) {
+      console.log("页面非激活");
+      isCurrentPageActive = false;
+    } else {
+      console.log("页面激活");
+      isCurrentPageActive = true;
+    }
+  };
+  document.addEventListener(visibilityChangeEvent, onVisibilityChange);
   async function doAllClassListen(obj) {
     for (let i = 0; i < obj.length; i++) {
       obj[i].click();
       console.log("点击按钮第" + i);
-      await sleepTime(60 * 60);
+      await sleepTime(20 * 60);
+      let minuteWait = 0;
+      while (!isCurrentPageActive && minuteWait <= 40) {
+        minuteWait++;
+        await sleepTime(1 * 60);
+      }
     }
   }
   setTimeout(function() {
