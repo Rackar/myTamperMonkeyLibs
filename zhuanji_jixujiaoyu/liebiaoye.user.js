bf50e84505e23d1f138362637f72822a914d9448
Rackar <27681135@qq.com>
2023-03-17 14:34:09 +0800

    完善列表页

--- zhuanji_jixujiaoyu/liebiaoye.user.js
+++ zhuanji_jixujiaoyu/liebiaoye.user.js
@@ -12,34 +12,6 @@
 (function () {
   "use strict";
 
-  // Your code here...
-  function sleepTime(secends) {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve(), secends * 1000);
-    });
-  }
-  async function doAllClassListen(obj) {
-    for (let i = 0; i < obj.length; i++) {
-      obj[i].button.click();
-      console.log("点击按钮第" + i);
-      await sleepTime(5);
-      console.log("等待5秒");
-      location.reload();
-      console.log("刷新页面");
-      await sleepTime(obj[i].time);
-    }
-  }
-  function gotTime(text) {
-    // let right = text.split("(")[1];
-    // let left = right.split(")")[0];
-    // console.log(left);
-    // let timearr = left.split(":");
-    // let secend = timearr[0] * 60 * 60 + timearr[1] * 60 + timearr[2] * 1 + 30;
-    let secend = text * 60 * 60;
-    console.log(secend);
-    return secend;
-  }
-
   if (
     location.hash.indexOf("v_selected_course") > -1 &&
     new Date().getFullYear() === 2023
@@ -48,35 +20,78 @@
       // $(".course-list.cb ul li a").attr("target", "_blank");
 
       // var trs = $(".course-list.cb ul li"); //2021以前的
-      let buttonDom = $(".course-list.cb ul li span.bg");
-      let timeDom = $(".course-list ul li .page-view span.tipColor");
-      let processDom = $(".course-list ul li div.progress-line span");
-      // console.log(buttonDom);
-      let arrNeed = [];
-      for (let index = 0; index < buttonDom.length; index++) {
-        const button = buttonDom[index];
-        const studyTime = timeDom[index];
-        let curProcess = processDom[index].innerText;
-        if (curProcess == "100%") {
-          continue;
-        }
-        // const aMark = tr.children[1];
-        //   console.log(tr.children[4].innerText);
-        if (studyTime.innerText.substring(0, 3) == "学时：") {
-          let time = gotTime(studyTime.innerText.slice(3));
-          // aMark.attr("target", "_blank");
-          let obj = {
-            time,
-            button,
-          };
-          arrNeed.push(obj);
-          // button.click()
-          // return;
-        }
+      let arrNeed = getList();
+      if (arrNeed && arrNeed.length) {
+        doAllClassListen(arrNeed);
+      } else {
+        getPage();
       }
-      console.log(arrNeed);
-
-      doAllClassListen(arrNeed);
     }, 4000);
   }
 })();
+function sleepTime(secends) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(), secends * 1000);
+  });
+}
+async function doAllClassListen(obj) {
+  for (let i = 0; i < obj.length; i++) {
+    obj[i].button.click();
+    console.log("点击按钮第" + i);
+    await sleepTime(5);
+    console.log("等待5秒");
+    location.reload();
+    console.log("刷新页面");
+    await sleepTime(obj[i].time);
+  }
+}
+function gotTime(text) {
+  let secend = text * 60 * 60;
+  console.log(secend);
+  return secend;
+}
+function getList() {
+  let buttonDom = $(".course-list.cb ul li span.bg");
+  let timeDom = $(".course-list ul li .page-view span.tipColor");
+  let processDom = $(".course-list ul li div.progress-line span");
+  // console.log(buttonDom);
+  let arrNeed = [];
+  for (let index = 0; index < buttonDom.length; index++) {
+    const button = buttonDom[index];
+    const studyTime = timeDom[index];
+    let curProcess = processDom[index].innerText;
+    if (curProcess == "100%") {
+      continue;
+    }
+    // const aMark = tr.children[1];
+    //   console.log(tr.children[4].innerText);
+    if (studyTime.innerText.substring(0, 3) == "学时：") {
+      let time = gotTime(studyTime.innerText.slice(3));
+      // aMark.attr("target", "_blank");
+      let obj = {
+        time,
+        button,
+      };
+      arrNeed.push(obj);
+      // button.click()
+      // return;
+    }
+  }
+  console.log(arrNeed);
+  return arrNeed;
+}
+
+async function getPage() {
+  let buttons = $(".el-pagination button.btn-next");
+  if (buttons[0] && buttons[0].click) {
+    buttons[0].click();
+    console.log("点击下一页");
+    await sleepTime(4);
+    let arrNeed = getList();
+    if (arrNeed && arrNeed.length) {
+      doAllClassListen(arrNeed);
+    } else {
+      getPage();
+    }
+  }
+}
