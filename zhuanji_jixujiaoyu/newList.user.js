e8b8c6f916bfa6d033f068616f7db61c54270c54
Rackar <27681135@qq.com>
2024-06-14 11:30:14 +0800

    注册继续教育搞定

--- zhuanji_jixujiaoyu/newList.user.js
+++ zhuanji_jixujiaoyu/newList.user.js
@@ -10,7 +10,19 @@
 // ==/UserScript==
 
 (function () {
-  "use strict";
+  ("use strict");
+
+  // 可以加 @run-at       document-idle
+  // 这条会等待页面全部加载后执行脚本
+
+  // 用户脚本的主体部分
+  document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOM已加载完毕。");
+  });
+
+  window.addEventListener("load", function () {
+    console.log("页面已完全加载完毕。");
+  });
 
   let btns = [];
   function sleepTime(secends) {
@@ -67,24 +79,45 @@
   }
 
   function clickUIbtn() {
-    // 获取页面上所有的按钮元素
-    var allButtons = document.getElementsByTagName("button");
+    var iframe = document.getElementById("bodyIframe");
+    if (iframe?.contentDocument?.readyState == "complete") {
+      var iframeDoc = iframe.contentWindow.document;
 
-    btns = [];
-    // 遍历所有按钮
-    for (var i = 0; i < allButtons.length; i++) {
-      // 检查按钮的文本内容是否为“立即学习”
-      if (allButtons[i].textContent === "立即学习") {
-        // 执行操作，例如添加点击事件监听器
-        btns.push(allButtons[i]);
-        // 可以在这里添加更多的操作
+      // 获取页面上所有的按钮元素
+      var allButtons = iframeDoc.getElementsByTagName("button");
+
+      btns = [];
+      // 遍历所有按钮
+      for (var i = 0; i < allButtons.length; i++) {
+        // 检查按钮的文本内容是否为“立即学习”
+        if (allButtons[i].textContent === "立即学习") {
+          // 执行操作，例如添加点击事件监听器
+          btns.push(allButtons[i]);
+          // 可以在这里添加更多的操作
+        }
+      }
+      if (btns.length == 0) {
+        alert("未找到立即学习按钮，是否未进入课程列表页？");
+      } else {
+        console.log(btns[0]);
+        btns[0].click();
       }
     }
-    if (btns.length == 0) {
-      alert("未找到立即学习按钮，是否未进入课程列表页？");
-    } else {
-      loopBtns(btns);
-    }
+    // 确保iframe加载完成后再进行操作
+    iframe.addEventListener("load", function () {});
+  }
+
+  function nextPage(params) {
+   
+    var iframe = document.getElementById("bodyIframe");
+    if (iframe?.contentDocument?.readyState == "complete") {
+      var iframeDoc = iframe.contentWindow.document;
+
+      // 获取页面上所有的按钮元素
+     var allButtons = iframeDoc.querySelectorAll(".layui-laypage-next");
+      if (allButtons && allButtons.length) {
+        allButtons[0].click();
+      }
   }
 
   async function loopBtns(btns) {
@@ -94,6 +127,46 @@
       await sleepTime(60 * 60);
     }
   }
+
+  function clickNode(node) {
+    var divText = node.textContent || node.innerText;
+    console.log("Detected popup div with text:", divText);
+    // 这里可以添加你想要执行的操作，比如发送文本到服务器等
+
+    // 使用querySelector来查找子孙中的链接，假设链接有一个特定的类名'your-link-class'
+    var link = node.querySelector(".layui-layer-btn0");
+
+    // 检查链接是否存在
+    if (link && link.innerText == "确定") {
+      link.click();
+      // 关闭弹窗
+      setTimeout(() => {
+        var closeBtns = document.body.querySelectorAll(".layui-layer-close");
+        if (closeBtns && closeBtns.length) {
+          closeBtns[0].click();
+        }
+        setTimeout(() => {
+          clickUIbtn();
+        }, 3000);
+      }, 3000);
+
+      // 模拟点击事件
+      //link.dispatchEvent(clickEvent);
+    } else {
+      console.log("Link not found");
+    }
+  }
+
+  function findDialog() {
+    var targetDiv = document.getElementsByClassName("layui-layer-dialog");
+    if (targetDiv && targetDiv.length) {
+      console.log("5秒遍历找到", targetDiv);
+      clickNode(targetDiv[0]);
+    }
+  }
+
+  setInterval(findDialog, 5 * 1000);
+
   function mainfun(btns) {
     var trs = $("tbody tr");
     // console.log("monkey");
@@ -133,7 +206,7 @@
 
     doAllClassListen(arrNeed);
   }
-  setTimeout(addUI, 4000);
+  setTimeout(addUI, 2000);
 
   // Your code here...
 })();
