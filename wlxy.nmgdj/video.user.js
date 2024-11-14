3dabe82fe38b70193063a879ad03f400f246434e
Rackar <27681135@qq.com>
2024-11-14 08:46:57 +0800

    加入分p完成检测和重试次数

--- wlxy.nmgdj/video.user.js
+++ wlxy.nmgdj/video.user.js
@@ -27,6 +27,25 @@
     }
   });
 
+  let loopInterval = null;
+  async function loopCheck() {
+    // 检查是否未播放完，同时有视频暂停状态
+    loopInterval = setInterval(checkOnce, 7000);
+  }
+
+  async function checkOnce() {
+    let video = document.querySelector("video");
+    if (video && video.paused) {
+      console.log("视频已暂停！");
+      let btn = document.querySelector(".next-dialog-footer button");
+      if (btn) {
+        btn.click();
+      } else {
+        startPlay();
+      }
+    }
+  }
+
   function sleepSec(sec) {
     return new Promise((resolve) => setTimeout(resolve, sec * 1000));
   }
@@ -43,18 +62,33 @@
     //   window.open("", "_self");
     //   window.close();
     // }
+    let index = parseInt(localStorage.getItem("list-start-index")) || 0;
+    localStorage.setItem("list-start-index", index + 1);
+    localStorage.setItem("retry-times", 0);
+    loopInterval = null;
     window.history.back();
     await sleepSec(5);
     console.log("等待5秒");
     location.reload();
     console.log("刷新页面");
   }
+  function checkRetryTimes() {
+    let retryTimes = parseInt(localStorage.getItem("retry-times")) || 0;
+    retryTimes++;
+    if (retryTimes > 7) {
+      closeCurrentPage();
+    } else {
+      localStorage.setItem("retry-times", retryTimes);
+    }
+  }
+
   async function startPlay() {
     if (checkCompletedState()) {
       return closeCurrentPage();
     } else {
       await skipFinishedVideo();
     }
+    checkRetryTimes();
     let v = document.querySelector("video");
     v.muted = true; //必须先静音，不然不给播放
     v.play();
@@ -62,8 +96,6 @@
       console.log("自定义监听：视频播放已结束！");
       await sleepSec(6);
       if (checkCompletedState()) {
-        let index = parseInt(localStorage.getItem("list-start-index")) || 0;
-        localStorage.setItem("list-start-index", index + 1);
         closeCurrentPage();
       }
 
@@ -85,7 +117,23 @@
     let current = split[0];
     let total = split[1];
     if (current == total) return true;
-    return false;
+    let list = document.querySelector(
+      '.simplebar-content div[class^="$id--player_side_item_wrap"]'
+    ).children;
+
+    for (let i = 0; i < list.length; i++) {
+      let item = list[i];
+      let percent = item.querySelector(
+        'div[class^="$id--player_side_item_progress"] span.ant-progress-text'
+      );
+      if (percent.innerText == "100%") {
+        continue;
+      } else {
+        return false;
+      }
+    }
+
+    return true;
   }
 
   async function skipFinishedVideo() {
@@ -110,5 +158,6 @@
 
   setTimeout(() => {
     startPlay();
+    loopCheck();
   }, 3 * 1000);
 })();
